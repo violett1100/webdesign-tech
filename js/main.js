efdc67d2061bba8d299ffad9c0b9beddfef5fd5f
violett1100 <violett1100@gmail.com>
2024-02-21 22:12:18 +0800

    First Upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    $('.carousel').carousel()
+    alert('此網站尚未完成，僅供展示使用，若有任何問題歡迎來信：\nviolett1100@gmail.com')
+}
+
+// 點擊a連結
+function demo() {
+    alert('此網站尚未完成，僅供展示使用，若有任何問題歡迎來信：\nviolett1100@gmail.com')
+}
